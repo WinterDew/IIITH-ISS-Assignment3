eecfb0e4981f226d5313c159213066f16442cfc5
Tatva Agarwal <tatva.agarwal.2006@gmail.com>
2025-04-15 19:14:42 +0530

    Fixed hamburger menu

--- main.js
+++ main.js
@@ -6,7 +6,18 @@ document.getElementById("footer-date").innerText = new Date().getFullYear();
 
 const hamburger = document.getElementById("hamburger");
 const nav = document.querySelector(".nav");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("active");
+  body.classList.toggle("no-scroll");
+});
+
+const links = document.querySelectorAll(".nav a");
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    
+    nav.classList.remove("active");
+    body.classList.remove("no-scroll");
+  });
 });
